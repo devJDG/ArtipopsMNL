d29bd730c380b759e85f7cc2396391a3ae9adf85
Jethro De Guzman <114161755+devJDG@users.noreply.github.com>
2024-12-02 11:27:17 +0800

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,23 @@
+function displayInfo() {
+        document.getElementById('info').innerHTML='₱ 675.00 <br> <br> Funko Pop! Marvel Spider-Man Across the Spiderverse <br> Spider-Punk with Protector';
+  }
+
+function closeInfo() {
+    document.getElementById('info').innerHTML='';
+}
+
+function displayInfo2() {
+  document.getElementById('info2').innerHTML='₱ 675.00 <br> <br> Funko Pop! One Piece <br> Roronoa Zoro with Protector';
+}
+
+function closeInfo2() {
+document.getElementById('info2').innerHTML='';
+}
+
+function displayInfo3() {
+  document.getElementById('info3').innerHTML='₱ 675.00 <br> <br> Funko Pop! One Piece <br> Luffy Gear Five with Protector';
+}
+
+function closeInfo3() {
+document.getElementById('info3').innerHTML='';
+}
